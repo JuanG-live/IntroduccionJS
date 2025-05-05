78503a4cfc250bef97739331dccf67f8777c6809
Juan Genova <juang@live.com.ar>
2025-05-05 15:59:53 +0100

    Templates Strings and Concatenation

---
+++ js/08.js
@@ -0,0 +1,7 @@
+// Templates Strings and Concatenation
+
+const product = 'Laptop';
+const price = 350;
+const type = 'Electronics';
+
+console.log(`The product is a ${product}, it costs $${price}, and it belongs to the ${type} category.`); 
