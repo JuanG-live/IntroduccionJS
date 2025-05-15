647a108778b4c0497ffc419c23fe174c643e35d1
Juan Genova <juang@live.com.ar>
2025-05-15 17:01:09 +0100

    Array Methods part 1

---
+++ js/17.js
@@ -0,0 +1,23 @@
+// Array methods
+
+const technology = ['HTML', 'CSS', 'node.js', 'Javascript', 'PHP', 'JAVA']
+const numbers = [1,2,3,4,5]
+
+//Filter
+/*
+const newArray = technology.filter((filterExample) => filterExample !== 'HTML');
+
+const newArrayTwo = technology.filter((filterExampleTwo) => console.log(filterExampleTwo));
+
+console.log(newArray);
+console.log(filterExampleTwo);
+
+const result = numbers.filter(numero => numero !== 1);
+console.log(result);
+*/
+
+//Includes 
+const result = technology.includes('CSS');
+console.log(result);
+
+
