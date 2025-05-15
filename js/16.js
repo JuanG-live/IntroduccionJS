655ed922f365825fcd0e63f96aba7ceb26ca19e4
Juan Genova <juang@live.com.ar>
2025-05-15 16:43:51 +0100

    Function that returns value 16

---
+++ js/16.js
@@ -0,0 +1,14 @@
+// Function that returns values
+
+// Function one
+const add = function (numberOne, numberTwo) {
+    return numberOne + numberTwo;
+}
+const addTwo = (numberOne, numberTwo) => numberOne + numberTwo;
+
+
+const result = add(10,20); 
+console.log(result);
+
+const resultTwo = addTwo(13,1)
+console.log(resultTwo);
