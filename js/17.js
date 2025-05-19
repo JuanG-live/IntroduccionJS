f8fb292d79f24b19d136b9cded0b60111065d2bc
Juan Genova <juang@live.com.ar>
2025-05-19 13:30:20 +0100

    Array methods Part 2

--- js/17.js
+++ js/17.js
@@ -3,7 +3,7 @@
 const technology = ['HTML', 'CSS', 'node.js', 'Javascript', 'PHP', 'JAVA']
 const numbers = [1,2,3,4,5]
 
-//Filter
+// FILTER
 /*
 const newArray = technology.filter((filterExample) => filterExample !== 'HTML');
 
@@ -16,8 +16,27 @@ const result = numbers.filter(numero => numero !== 1);
 console.log(result);
 */
 
-//Includes 
-const result = technology.includes('CSS');
+// INCLUDES 
+/*const result = technology.includes('CSS');
 console.log(result);
+*/
 
+// SOME - Return true if at least one meets the condition.
+/*const result = numbers.some(number => number > 3);
+console.log(result);
+*/
 
+// FIND - Return the first element that meets the condition
+/*const result = numbers.find( number => number >3)
+console.log(result); -> 4
+*/
+
+// EVERY - Return true if all elements meets the condition, otherwise false
+/*const result = numbers.every(number => number > 3)
+console.log(result); -> FALSE
+*/
+
+// REDUCE - Return the total sum
+/* const result = numbers.reduce((total, number) => total + number, 0)
+console.log(result); -> 15
+*/
