8fcb1590d096dff662f7f50f8ec741c8c469a378
Juan Genova <juang@live.com.ar>
2025-05-19 13:53:36 +0100

    Conditional Logical

---
+++ js/20.js
@@ -0,0 +1,17 @@
+const count = 500
+const pay = 1000
+const target = false
+
+/**
+ * 
+ * Logical OR and Logical AND
+ * || or 
+ * && and 
+ * 
+ */
+
+if (pay > count || target) {
+    console.log('You can pay')
+} else {
+    console.log('You cannot pay')
+}
