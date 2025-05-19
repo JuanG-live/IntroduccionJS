24dd536bff8175cf3b06ac8ada6dd1832c177077
Juan Genova <juang@live.com.ar>
2025-05-19 13:47:21 +0100

    Operator Conditional

---
+++ js/19.js
@@ -0,0 +1,17 @@
+const numberOne = 20;
+const numberTwo = "20";
+
+/*
+=== Strict equality operator
+==  loose equality operator
+*/
+
+console.log(typeof numberOne)
+console.log(typeof numberTwo)
+
+if(numberOne === numberTwo){
+    console.log('Is the same')
+} else{
+    console.log('Is not the same')
+}
+
