38492cb25b72ecd788b2f35537e3ac830d1d02e7
Juan Genova <juang@live.com.ar>
2025-05-26 15:08:58 +0100

    ECMAScript Modules

---
+++ js/24.js
@@ -0,0 +1,7 @@
+import { add, subtract } from './function.js'
+
+const result = add(1,3)
+console.log(result)
+
+const resultTwo = subtract(4,1)
+console.log(resultTwo)
---
+++ js/function.js
@@ -0,0 +1,8 @@
+export function add(numberOne, numberTwo) {
+    return numberOne + numberTwo
+}
+
+export function subtract(numberOne, numberTwo) {
+    return numberOne - numberTwo
+}
+
