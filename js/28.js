28733ae564b298662d31655fefc4f6d156de9af3
Juan Genova <juang@live.com.ar>
2025-06-07 18:55:09 +0100

    40. Selectors

---
+++ js/28.js
@@ -0,0 +1,15 @@
+// Selectors
+
+const heading = document.querySelector('.heading')
+
+//console.log(heading)
+//console.log(heading.tagName)
+//console.log(heading.textContent)
+//console.log(heading.classList)
+//console.log(heading.id)
+
+const url = document.querySelector('.navegacion a')
+const urlTwo = document.querySelectorAll('.navegacion a')
+
+console.log(url)
+console.log(urlTwo)
